729d70115ad70070a6377dcdd7517e4410e84ff8
Josimar Silva <josimaralves.ti@gmail.com>
2025-07-23 16:46:22 +0200

    perf: instrument performance data collection

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
 use std::sync::Arc;
 
+#[cfg(feature = "perf")]
+use pprof::flamegraph::Options;
 use rinha_de_backend::infrastructure::config::settings::Config;
 use rinha_de_backend::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+	#[cfg(feature = "perf")]
+	let guard = pprof::ProfilerGuardBuilder::default()
+		.frequency(1000)
+		.blocklist(&["libc", "libgcc", "pthread", "vdso"])
+		.build()
+		.unwrap();
+
 	let config = Arc::new(Config::load().expect("Failed to load configuration"));
-	run(config).await
+	let result = run(config).await;
+
+	#[cfg(feature = "perf")]
+	if let Ok(report) = guard.report().build() {
+		let mut file = std::fs::File::create("flamegraph.svg").unwrap();
+		let mut options = Options::default();
+		options.title = "rinha-de-backend".to_string();
+		options.count_name = "samples".to_string();
+		report
+			.flamegraph_with_options(&mut file, &mut options)
+			.unwrap();
+	}
+
+	result
 }
